a97af55c183f4c67c3bec5eb53e93ad692ca9fe8
Marvinag <33891771+Marvinag@users.noreply.github.com>
2020-05-29 09:55:46 +0300

    Add files via upload

---
+++ county_mix.php
@@ -0,0 +1,409 @@
+<?php
+/**
+  Counties in the Republic of Kenya,Governors and their Political Parties.
+*/
+
+$county_data = "1. Mombasa. – Hassan Ali Joho – ODM,
+2. Kwale. – Salim Mvurya – Jubilee,
+3. Kilifi. – Amason Jeffah Kingi – ODM,
+4. Tana River. – Dhadho Godhana – ODM,
+5. Lamu. – Fahim Yasin Twaha – Jubilee,
+6. Taita-Taveta. – Granton Samboja – WDM K,
+7. Garissa. – Ali Bunow Korane – Jubilee,
+8. Wajir. – Mohamed Abdi Mohamud – Jubilee,
+9. Mandera. – Ali Ibrahim Roba - Jubilee,
+10. Marsabit – Mohamud Mohamed Ali – Jubilee,
+11. Isiolo – Mohamed Abdi Kuti – Independent,
+12. Meru – Kiraitu Murungi – Jubilee,
+13. Tharaka Nithi – Onesmus Muthomi Njuki – Jubilee,
+14. Embu – Martin Nyaga Wambora – Jubilee,
+15. Kitui – Charitu Ngilu – Narc Kenya,
+16. Machakos – Alfred Mutua – CCM,
+17. Makueni – Kivutha Kibwana – WDM K.,
+18. Nyandarua – Francis Kimemia – Jubilee,
+19. Nyeri – Mutahi Kahiga – Jubilee,
+20. Kirinyaga. – Ann Waiguru – Jubilee,
+21. Murang’a – Mwangi wa iria – Jubilee,
+22. Kiambu. – Ferdinand Waititu Babayao – Jubilee,
+23. Turkana. – Josphat Nanok – ODM,
+24. West Pokot. – John Krop Lonyang’apuo. – KANU,
+25. Samburu. – Moses Kasainie Lenolkulal – Jubilee,
+26. Trans-Nzoia. – Patrick Khaemba – FORD Kenya,
+27. Uasin gishu. – Jackson Mandago – Jubilee,
+28. Elgeyo Marakwet. – Alex Tanui Tolgas – Jubilee,
+29. Nandi. – Stephen Sang – Jubilee,
+30. Baringo – Stanley K Kipris. – Jubilee,
+31. Laikipia – Ndiritu Muriithi – Independent,
+32. Nakuru. – Lee Kinyanjui – Jubilee,
+33. Narok – Samuel Kuntai Ole Tunai – Jubilee,
+34. Kajiado. – Joseph Ole Lenku – Jubilee,
+35. Kericho. – Prof.Paul Chepkwony Kiprono – Jubilee,
+36. Bomet. – Hillary Barchok – Jubilee,
+37. Kakamega. – Wyclife Oparanya – ODM,
+38. Vihiga. – Wilber Ottichilo – ODM,
+39. Bungoma. – Wyclife Wafula Wangamiti – ODM,
+40. Busia. – Sospeter Ojaamong – ODM,
+41. Siaya. – Cornel Rasanga. – ODM,
+42. Kisumu. – Peter Anyang Nyong’o. – ODM,
+43. Homabay. – Cyprian Awiti – ODM,
+44. Migori. – Zachary Okoth Obado – ODM,
+45. Kisii. – James Ongware – ODM,
+46. Nyamira. – John Nyangarama Obiena - ODM,
+47. Nairobi. – Mike Sonko Mbuvi Kioko – Jubilee";
+
+/**
+ You should not modify anything above this line
+*/
+
+// @TODO : Your code starts here
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Counties, Governors and websites</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+</head>
+<body>
+<div class="container">
+    <table class="table is-bordered is-striped is-fullwidth">
+        <tr class="th is-selected">
+            <th>#</th>
+            <th>County Name</th>
+            <th>Governor</th>
+            <th>Website</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Mombasa</td>
+            <td>Ali Hassan Joho<p style="float: left;"><img src="images/odm.png" height="20px" width="20px" ></p></td>
+            <td>http://www.mombasa.go.ke</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Kwale</td>
+            <td>Salim Mvurya<p style="float: left;"><img src="images/Jubilee.png" height="20px" width="20px" ></p></td>
+            <td>http://www.kwalecountygov.com</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Kilifi</td>
+            <td>Amason Jeffah Kingi<p style="float: left;"><img src="images/odm.png" height="20px" width="20px" ></p></td>
+            <td>http://www.kilifi.go.ke</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Tana River</td>
+            <td>Dhadho Godhana<p style="float: left;"><img src="images/odm.png" height="20px" width="20px" ></p></td>
+            <td>http://www.tanariver.go.ke</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Lamu</td>
+            <td>Fahim Yasin Twaha<p style="float: left;"><img src="images/Jubilee.png" height="20px" width="20px" ></p></td>
+            <td>http://www.lamu.go.ke</td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td>Taita-Taveta</td>
+            <td>Granton Samboja<p style="float: left;"><img src="images/wiper.png" height="20px" width="20px" ></p></td>
+            <td>http://taitataveta.go.ke</td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>Garissa</td>
+            <td>Ali Bunow Korane<p style="float: left;"><img src="images/Jubilee.png" height="20px" width="20px" ></p></td>
+            <td>http://garissa.go.ke</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>Wajir</td>
+            <td>Mohamed Abdi Mohamud<p style="float: left;"><img src="images/Jubilee.png" height="20px" width="20px" ></p></td>
+            <td>http://www.wajir.go.ke</td>
+        </tr>
+        <tr>
+            <td>9</td>
+            <td>Mandera</td>
+            <td>Ali Ibrahim Roba<p style="float: left;"><img src="images/Jubilee.png" height="20px" width="20px" ></p></td>
+            <td>http://www.mandera.go.ke</td>
+        </tr>
+        <tr>
+            <td>10</td>
+            <td>Marsabit</td>
+            <td>Mohamud Mohamed Ali<p style="float: left;"><img src="images/Jubilee.png" height="20px" width="20px" ></p></td>
+            <td>http://marsabit.go.ke</td>
+        </tr>
+        <tr>
+            <td>11</td>
+            <td>Isiolo</td>
+            <td>Mohamed Abdi Kuti</td>
+            <td>http://www.isiolo.go.ke</td>
+        </tr>
+        <tr>
+            <td>12</td>
+            <td>Meru</td>
+            <td>Kiraitu Murungi<p style="float: left;"><img src="images/Jubilee.png" height="20px" width="20px" ></p></td>
+            <td>http://meru.go.ke</td>
+        </tr>
+        <tr>
+            <td>13</td>
+            <td>Tharaka Nithi</td>
+            <td>Onesmus Muthomi Njuki<p style="float: left;"><img src="images/Jubilee.png" height="20px" width="20px" ></p></td>
+            <td>http://www.tharakanithi.go.ke</td>
+        </tr>
+        <tr>
+            <td>14</td>
+            <td>Embu </td>
+            <td>Martin Nyaga Wambora<p style="float: left;"><img src="images/Jubilee.png" height="20px" width="20px" ></p></td>
+            <td>http://www.embu.go.ke</td>
+        </tr>
+        <tr>
+            <td>15</td>
+            <td>Kitui </td>
+            <td>Charitu Ngilu<p style="float: left;"><img src="images/Narc-Kenya.jpg" height="20px" width="20px" ></p></td>
+            <td>http://www.kitui.go.ke</td>
+        </tr>
+        <tr>
+            <td>16</td>
+            <td>Machakos</td>
+            <td>Alfred Mutua<p style="float: left;"><img src="images/ccm.jpg" height="20px" width="20px" ></p></td>
+            <td>http://www.machakosgovernment.com</td>
+        </tr>
+        <tr>
+            <td>17</td>
+            <td>Makueni </td>
+            <td>Kivutha Kibwana<p style="float: left;"><img src="images/wiper.png" height="20px" width="20px"></p></td>
+            <td>http://www.makueni.go.ke</td>
+        </tr>
+        <tr>
+            <td>18</td>
+            <td>Nyandarua </td>
+            <td>Francis Kimemia<p style="float: left;"><img src="images/Jubilee.png" height="20px" width="20px"></p></td>
+            <td>http://www.nyandarua.go.ke</td>
+        </tr>
+        <tr>
+            <td>19</td>
+            <td>Nyeri</td>
+            <td>Mutahi Kahiga<p style="float: left;"><img src="images/Jubilee.png" height="20px" width="20px"></p></td>
+            <td>http://www.nyeri.go.ke</td>
+        </tr>
+        <tr>
+            <td>20</td>
+            <td>Kirinyaga</td>
+            <td>Ann Waiguru<p style="float: left;"><img src="images/Jubilee.png" height="20px" width="20px"></p></td>
+            <td>http://www.kirinyaga.go.ke</td>
+        </tr>
+        <tr>
+            <td>21</td>
+            <td>Murang'a</td>
+            <td>Mwangi wa iria<p style="float: left;"><img src="images/Jubilee.png" height="20px" width="20px"></p></td>
+            <td>http://muranga.go.ke</td>
+        </tr>
+        <tr>
+            <td>22</td>
+            <td>Kiambu</td>
+            <td>Ferdinand Waititu Babayao<p style="float: left;"><img src="images/Jubilee.png" height="20px" width="20px"></p></td>
+            <td>http://www.kiambu.go.ke</td>
+        </tr>
+        <tr>
+            <td>23</td>
+            <td>Turkana</td>
+            <td>Josphat Nanok<p style="float: left;"><img src="images/odm.png" height="20px" width="20px"></p></td>
+            <td>http://www.turkana.go.ke</td>
+        </tr>
+        <tr>
+            <td>24</td>
+            <td>West Pokot</td>
+            <td>John Krop Lonyang’apuo<p style="float: left;"><img src="images/kanu.png" height="20px" width="20px"></p></td>
+            <td>http://www.westpokot.go.ke</td>
+        </tr>
+        <tr>
+            <td>25</td>
+            <td>Samburu</td>
+            <td>Moses Kasainie Lenolkulal<p style="float: left;"><img src="images/Jubilee.png" height="20px" width="20px"></p></td>
+            <td>http://www.samburu.go.ke</td>
+        </tr>
+        <tr>
+            <td>26</td>
+            <td>Trans-Nzoia</td>
+            <td>Patrick Khaemba<p style="float: left;"><img src="images/ford-kenya.png" height="20px" width="20px"></p></td>
+            <td>https://www.transnzoia.go.ke</td>
+        </tr>
+        <tr>
+            <td>27</td>
+            <td>Uasin gishu</td>
+            <td>Jackson Mandago<p style="float: left;"><img src="images/Jubilee.png" height="20px" width="20px"></p></td>
+            <td>http://uasingishu.go.ke</td>
+        </tr>
+        <tr>
+            <td>28</td>
+            <td>Elgeyo Marakwet</td>
+            <td>Alex Tanui Tolgas<p style="float: left;"><img src="images/Jubilee.png" height="20px" width="20px"></p></td>
+            <td>http://www.elgeyomarakwet.go.ke</td>
+        </tr>
+        <tr>
+            <td>29</td>
+            <td>Nandi</td>
+            <td>Stephen Sang<p style="float: left;"><img src="images/Jubilee.png" height="20px" width="20px"></p></td>
+            <td>http://nandi.go.ke</td>
+        </tr>
+        <tr>
+            <td>30</td>
+            <td>Baringo </td>
+            <td>Stanley K Kipris<p style="float: left;"><img src="images/Jubilee.png" height="20px" width="20px"></p></td>
+            <td>http://www.baringo.go.ke</td>
+        </tr>
+        <tr>
+            <td>31</td>
+            <td>Laikipia </td>
+            <td>Ndiritu Muriithi</td>
+            <td>http://www.laikipiacounty.go.ke</td>
+        </tr>
+        <tr>
+            <td>32</td>
+            <td>Nakuru</td>
+            <td>Lee Kinyanjui<p style="float: left;"><img src="images/Jubilee.png" height="20px" width="20px"></p></td>
+            <td>http://www.nakuru.go.ke</td>
+        </tr>
+        <tr>
+            <td>33</td>
+            <td>Narok</td>
+            <td>Samuel Kuntai Ole Tunai<p style="float: left;"><img src="images/Jubilee.png" height="20px" width="20px"></p></td>
+            <td>http://www.narok.go.ke</td>
+        </tr>
+        <tr>
+            <td>34</td>
+            <td>Kajiado</td>
+            <td>Joseph Ole Lenku<p style="float: left;"><img src="images/Jubilee.png" height="20px" width="20px"></p></td>
+            <td>https://www.kajiado.go.ke</td>
+        </tr>
+        <tr>
+            <td>35</td>
+            <td>Kericho</td>
+            <td>Prof.Paul Chepkwony Kiprono<p style="float: left;"><img src="images/Jubilee.png" height="20px" width="20px"></p></td>
+            <td>http://kericho.go.ke</td>
+        </tr>
+        <tr>
+            <td>36</td>
+            <td>Bomet</td>
+            <td>Hillary Barchok<p style="float: left;"><img src="images/Jubilee.png" height="20px" width="20px"></p></td>
+            <td>http://www.bomet.go.ke</td>
+        </tr>
+        <tr>
+            <td>37</td>
+            <td>Kakamega</td>
+            <td>Wyclife Oparanya<p style="float: left;"><img src="images/odm.png" height="20px" width="20px"></p></td>
+            <td>http://www.kakamega.go.ke</td>
+        </tr>
+        <tr>
+            <td>38</td>
+            <td>Vihiga</td>
+            <td>Wilber Ottichilo<p style="float: left;"><img src="images/odm.png" height="20px" width="20px"></p></td>
+            <td>http://vihiga.go.ke</td>
+        </tr>
+        <tr>
+            <td>39</td>
+            <td>Bungoma</td>
+            <td>Wyclife Wafula Wangamiti<p style="float: left;"><img src="images/odm.png" height="20px" width="20px"></p></td>
+            <td>http://www.bungoma.go.ke</td>
+        </tr>
+        <tr>
+            <td>40</td>
+            <td>Busia</td>
+            <td>Sospeter Ojaamong<p style="float: left;"><img src="images/odm.png" height="20px" width="20px"></p></td>
+            <td>http://www.busiacounty.go.ke</td>
+        </tr>
+        <tr>
+            <td>41</td>
+            <td>Siaya</td>
+            <td>Cornel Rasanga<p style="float: left;"><img src="images/odm.png" height="20px" width="20px"></p></td>
+            <td>http://www.siaya.go.ke</td>
+        </tr>
+        <tr>
+            <td>42</td>
+            <td>Kisumu</td>
+            <td>Peter Anyang Nyong’o<p style="float: left;"><img src="images/odm.png" height="20px" width="20px"></p></td>
+            <td>http://kisumu.go.ke</td>
+        </tr>
+        <tr>
+            <td>43</td>
+            <td>Homabay</td>
+            <td>Cyprian Awiti<p style="float: left;"><img src="images/odm.png" height="20px" width="20px"></p></td>
+            <td>http://homabay.go.ke</td>
+        </tr>
+        <tr>
+            <td>44</td>
+            <td>Migori</td>
+            <td>Zachary Okoth Obado<p style="float: left;"><img src="images/odm.png" height="20px" width="20px"></p></td>
+            <td>http://migori.go.ke/</td>
+        </tr>
+        <tr>
+            <td>45</td>
+            <td>Kisii</td>
+            <td>James Ongware<p style="float: left;"><img src="images/odm.png" height="20px" width="20px"></p></td>
+            <td>http://www.kisii.go.ke</td>
+        </tr>
+        <tr>
+            <td>46</td>
+            <td>Nyamira</td>
+            <td>John Nyangarama Obiena<p style="float: left;"><img src="images/odm.png" height="20px" width="20px"></p></td>
+            <td>http://www.nyamira.go.ke</td>
+        </tr>
+        <tr>
+            <td>47</td>
+            <td>Nairobi</td>
+            <td>Mike Sonko Mbuvi Kioko<p style="float: left;"><img src="images/Jubilee.png" height="20px" width="20px"></p></td>
+            <td>http://www.nairobi.go.ke</td>
+        </tr>
+
+
+    </table>
+
+    <tfoot>
+
+    <!DOCTYPE html>
+    <html lang="en-US">
+    <body>
+
+
+    <div id="piechart"></div>
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+    <script type="text/javascript">
+        // Load google charts
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+
+        // Draw the chart and set the chart values
+        function drawChart() {
+            var data = google.visualization.arrayToDataTable([
+                ['Political party', 'Number in counties'],
+                ['Odm', 14],
+                ['Jubilee', 25],
+                ['Narc-Kenya', 1],
+                ['CCM', 1],
+                ['Independent', 2],
+                ['Ford-Kenya', 1],
+                ['Kanu', 1],
+                ['Wiper', 2]
+            ]);
+
+            // Optional; add a title and set the width and height of the chart
+            var options = {'title':'Party distribution in Counties', 'width':850, 'height':650};
+
+            // Display the chart inside the <div> element with id="piechart"
+            var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+            chart.draw(data, options);
+        }
+    </script>
+
+    </body>
+    </html>
+    </tfoot>
+
+</div>
+
+</body>
+</html>
+
